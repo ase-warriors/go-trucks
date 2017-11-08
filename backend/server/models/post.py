e38c0d0129c5ac5b3679fc21f1eb7a6a7908478e
Ruoxin Jiang <rj2394@columbia.edu>
2017-11-07 22:26:38 -0500

    backend: return only latestest post within a distance in get_post

--- backend/server/models/post.py
+++ backend/server/models/post.py
@@ -3,14 +3,13 @@
 import datetime
 from server import app, db
 from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION
+from sqlalchemy.sql import func, label
 
 
 class Post(db.Model):
     """ User Model for storing user related details """
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    vendor_id = db.Column(
-        db.Integer, db.ForeignKey("vendors.id"), nullable=False)
     location = db.Column(db.String(500), nullable=False)
     lat = db.Column(DOUBLE_PRECISION, nullable=False)
     lng = db.Column(DOUBLE_PRECISION, nullable=False)
@@ -19,6 +18,9 @@ class Post(db.Model):
     time = db.Column(db.Text, nullable=False)
     menu = db.Column(db.Text, nullable=True)
     posted_on = db.Column(db.DateTime, nullable=False)
+
+    vendor_id = db.Column(
+        db.Integer, db.ForeignKey("vendors.id"), nullable=False)
 
     def __init__(self, vendor_id, location, lat, lng, time, menu=None):
         self.vendor_id = vendor_id
@@ -57,10 +59,19 @@ class Post(db.Model):
 
     @staticmethod
     def get_post_list(lat, lng, distance):
-        # TODO(amy): only latest post from vendor_id
-        posts = Post.query.from_statement(
-            db.text(
-                "SELECT * FROM posts "
-                "WHERE earth_box(ll_to_earth(:lat, :lng), :distance) @> ll_to_earth(lat, lng);"
-            ).params(lat=lat, lng=lng, distance=distance)).all()
+        # Filter the latest post of each vendor
+        subquery = db.session.query(
+            Post,
+            func.max(Post.posted_on).over(
+                partition_by=Post.vendor_id).label('max')).subquery()
+        latest_posts = db.session.query(subquery).filter(
+            subquery.c.max == subquery.c.posted_on)
+
+        if distance == None:
+            return latest_posts.all()
+
+        loc = func.ll_to_earth(lat, lng)
+        loc_post = func.ll_to_earth(subquery.c.lat, subquery.c.lng)
+        dist_func = func.earth_distance(loc, loc_post)
+        posts = latest_posts.filter(dist_func <= distance).all()
         return posts
