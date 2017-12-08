f77df0bf228bd7b471ae062f26b2a89d132b1fef
Ruoxin Jiang <rj2394@columbia.edu>
2017-12-08 12:04:02 -0500

    backend: add coverage support

--- backend/manager.py
+++ backend/manager.py
@@ -1,9 +1,22 @@
-import unittest
+""" Manager.py runs commands to run server and create/destroy db."""
 
-from flask_script import Manager, Server
+import os
+import unittest
+import coverage
+
+from flask_script import Manager
 from server import app, db
 
 manager = Manager(app)
+
+COV = coverage.coverage(
+    branch=True,
+    include="server/*",
+    omit=[
+        "server/*/__init__.py"
+    ]
+)
+COV.start()
 
 @manager.command
 def create_db():
@@ -18,8 +31,26 @@ def test():
     tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=1).run(tests)
     if result.wasSuccessful():
-       return 0
+        return 0
     return 1
+
+@manager.command
+def cov():
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print("Coverage Summary:")
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'coverage')
+        COV.html_report(directory=covdir)
+        COV.erase()
+        return 0
+    return 1
+
+
 
 if __name__ == "__main__":
     manager.run()
